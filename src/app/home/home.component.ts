eac984765eb598e32425f5929e7199c7dafa408d
Pragadeeswaran5 <121494659+Pragadeeswaran5@users.noreply.github.com>
2022-12-27 18:02:17 +0530

    Add files via upload

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,38 @@
+﻿import { Component } from '@angular/core';
+
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent {
+    user;
+    cinemas = [{
+        image: 'https://images.theconversation.com/files/501182/original/file-20221214-14389-tahjkr.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip',
+        name: 'Avatar'
+    },{
+        image: 'https://images.theconversation.com/files/501182/original/file-20221214-14389-tahjkr.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip',
+        name: 'Quiet'
+    },{
+        image: 'https://images.theconversation.com/files/501182/original/file-20221214-14389-tahjkr.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip',
+        name: 'Avengers'
+    },{
+        image: 'https://images.theconversation.com/files/501182/original/file-20221214-14389-tahjkr.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip',
+        name: 'Fall'
+    },{
+        image: 'https://images.theconversation.com/files/501182/original/file-20221214-14389-tahjkr.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip',
+        name: 'Interstellar'
+    },{
+        image: 'https://images.theconversation.com/files/501182/original/file-20221214-14389-tahjkr.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip',
+        name: 'Inception'
+    }]
+    cinemaFilteredList = this.cinemas
+    constructor() {
+    }
+
+    search(evn) {
+        let text = evn.target.value
+        if(text == '') {
+            this.cinemaFilteredList = this.cinemas
+        } else {
+            this.cinemaFilteredList = this.cinemas.filter((c) => c.name.toLowerCase().includes(text.toLowerCase()))
+        }
+    }
+}
